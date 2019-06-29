ab14ce0ad1d01c3c5393f17933648310e9726b65
Chopper <ilua292010@gmail.com>
2019-06-29 19:03:47 +0300

    1.0

---
+++ precache-manifest.67e20c4dace2539d94eaf1b36889c0c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c00a45fbf11563796c1731c8bc379ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86e5249cc2bdc2029e0b",
+    "url": "/static/css/main.a6c33aca.chunk.css"
+  },
+  {
+    "revision": "20464f9387033a85fdd9",
+    "url": "/static/js/2.514bae87.chunk.js"
+  },
+  {
+    "revision": "86e5249cc2bdc2029e0b",
+    "url": "/static/js/main.881603c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "768d66ac1a00fd754d54294f118c65d2",
+    "url": "/static/media/bg3.768d66ac.jpg"
+  },
+  {
+    "revision": "60f3818e1ff9abe4f9974cca063af2d9",
+    "url": "/static/media/skills2.60f3818e.png"
+  }
+]);
